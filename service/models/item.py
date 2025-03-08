ba9f6f54de19fca4b12eafb8b5f6c9de71cc20e6
Brian <bleibowitz13@gmail.com>
2025-03-08 17:09:59 +0000

    Set up dbs

---
+++ service/models/item.py
@@ -0,0 +1,86 @@
+######################################################################
+# Copyright 2016, 2024 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Persistent Base class for database CRUD functions
+"""
+
+import logging
+from .persistent_base import db, PersistentBase, DataValidationError
+
+logger = logging.getLogger("flask.app")
+
+
+######################################################################
+#  A D D R E S S   M O D E L
+######################################################################
+class Item(db.Model, PersistentBase):
+    """
+    Class that represents an Item
+    """
+
+    # Table Schema
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(63))
+    shopcart_id = db.Column(
+        db.Integer, db.ForeignKey("shopcart.id", ondelete="CASCADE"), nullable=False
+    )
+    description = db.Column(db.String(63), nullable=False)
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"<Item {self.name} id=[{self.id}] shopcart[{self.shopcart_id}]>"
+
+    def __str__(self):
+        return f"{self.name}: {self.description}, {self.price}, {self.quantity}"
+
+    def serialize(self) -> dict:
+        """Converts an Item into a dictionary"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "shopcart_id": self.shopcart_id,
+            "description": self.description,
+            "quantity": self.quantity,
+            "price": self.price,
+        }
+
+    def deserialize(self, data: dict) -> None:
+        """
+        Populates an Item from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.shopcart_id = data["shopcart_id"]
+            self.name = data["name"]
+            self.price = data["price"]
+            self.quantity = data["quantity"]
+            self.description = data["description"]
+        except AttributeError as error:
+            raise DataValidationError("Invalid attribute: " + error.args[0]) from error
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid Item: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Item: body of request contained bad or no data " + str(error)
+            ) from error
+
+        return self
---
+++ service/models/shopcart.py
@@ -0,0 +1,97 @@
+"""
+Models for Shopcart
+
+All of the models are stored in this module
+"""
+
+import logging
+from flask_sqlalchemy import SQLAlchemy
+
+from .persistent_base import db, PersistentBase, DataValidationError
+
+logger = logging.getLogger("flask.app")
+
+# Create the SQLAlchemy object to be initialized later in init_db()
+db = SQLAlchemy()
+
+
+class DataValidationError(Exception):
+    """Used for an data validation errors when deserializing"""
+
+
+class Shopcart(db.Model):
+    """
+    Class that represents a Shopcart
+    """
+
+    ### Translator
+    # Shopcart = Account
+    # Item = Address
+
+    ##################################################
+    # Table Schema
+    ##################################################
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer)
+    items = db.relationship("Item", backref="shopcart", passive_deletes=True)
+    time_atc = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f"<Shopcart {self.customer_id} id=[{self.id}]>"
+
+    def serialize(self):
+        """Serializes a Shopcart into a dictionary"""
+        shopcart = {
+            "id": self.id,
+            "customer_id": self.customer_id,
+            "time_atc": self.time_atc,
+            "items": [],
+        }
+        for item in self.items:
+            shopcart["items"].append(item.serialize())
+            return shopcart
+
+    def deserialize(self, data):
+        """
+        Deserializes a Shopcart from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.customer_id = data["customer_id"]
+            self.time_atc = data["time_atc"]
+            # handle inner list of items
+            item_list = data.get("item_id")
+            for json_item in item_list:
+                item = Item()
+                item.deserialize(json_item)
+                item.shopcart_id = self.id
+                self.item.append(item)
+
+        except AttributeError as error:
+            raise DataValidationError("Invalid attribute: " + error.args[0]) from error
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid Shopcart: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Shopcart: body of request contained bad or no data "
+                + str(error)
+            ) from error
+        return self
+
+    ##################################################
+    # CLASS METHODS
+    ##################################################
+
+    @classmethod
+    def find_by_customer(cls, customer_id):
+        """Returns all Shopcarts with the given customer
+
+        Args:
+            customer_id (int): the customer ID of the Shopcarts you want to match
+        """
+        logger.info("Processing name query for %s ...", customer_id)
+        return cls.query.filter(cls.customer_id == customer_id)
