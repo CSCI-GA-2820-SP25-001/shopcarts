cd1f0e0a7fdb8677d531c46edc2ed5b1cfd8bcac
nglahani <nlahanis27@gmail.com>
2025-03-17 20:04:07 +0000

    Test Cases Adjusted (Based on sample-accounts repo)

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -15,7 +15,7 @@
 ######################################################################
 
 """
-TestShopcart API Service Test Suite
+Test Shopcart API Service Test Suite
 """
 
 # pylint: disable=duplicate-code
@@ -23,6 +23,7 @@ import os
 import logging
 from unittest import TestCase
 from wsgi import app
+from tests.factories import ShopcartFactory, ItemFactory
 from service.common import status
 from service.models import db, Shopcart
 
@@ -30,12 +31,14 @@ DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
 )
 
+BASE_URL = "/shopcarts"
+
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceService(TestCase):
+class TestShopcartService(TestCase):
     """REST API Server Tests"""
 
     @classmethod
@@ -64,12 +67,302 @@ class TestYourResourceService(TestCase):
         db.session.remove()
 
     ######################################################################
-    #  P L A C E   T E S T   C A S E S   H E R E
+    #  H E L P E R   M E T H O D S
+    ######################################################################
+
+    def _create_shopcarts(self, count):
+        """Factory method to create shopcarts in bulk"""
+        shopcarts = []
+        for _ in range(count):
+            shopcart = ShopcartFactory()
+            resp = self.client.post(BASE_URL, json=shopcart.serialize())
+            self.assertEqual(
+                resp.status_code,
+                status.HTTP_201_CREATED,
+                "Could not create test Shopcart",
+            )
+            new_shopcart = resp.get_json()
+            shopcart.id = new_shopcart["id"]
+            shopcarts.append(shopcart)
+        return shopcarts
+
+    ######################################################################
+    #  SHOPCART  T E S T   C A S E S
     ######################################################################
 
     def test_index(self):
-        """It should call the home page"""
+        """It should call the Home Page"""
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    # Todo: Add your test cases here...
+    def test_get_shopcart_list(self):
+        """It should Get a list of shopcarts"""
+        self._create_shopcarts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_get_shopcart_by_customer_id(self):
+        """It should Get an shopcart by customer_id"""
+        shopcarts = self._create_shopcarts(3)
+        resp = self.client.get(
+            BASE_URL, query_string=f"customer_id={shopcarts[1].customer_id}"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data[0]["customer_id"], shopcarts[1].customer_id)
+
+    def test_get_shopcart(self):
+        """It should Read a single shopcart"""
+        # get the id of an shopcart
+        shopcart = self._create_shopcarts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{shopcart.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["customer_id"], shopcart.customer_id)
+
+    def test_get_shopcart_not_found(self):
+        """It should not Read an shopcart that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_shopcart(self):
+        """It should Create a new shopcart"""
+        shopcart = ShopcartFactory()
+        resp = self.client.post(
+            BASE_URL, json=shopcart.serialize(), content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = resp.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        # Check the data is correct
+        new_shopcart = resp.get_json()
+
+        self.assertEqual(new_shopcart["id"], shopcart.id, "Shopcart IDs do not match")
+        self.assertEqual(
+            new_shopcart["customer_id"],
+            shopcart.customer_id,
+            "Customer IDs do not match",
+        )
+        self.assertEqual(
+            new_shopcart["time_atc"], shopcart.time_atc, "Times do not match"
+        )
+
+        # Check that the location header was correct by getting it
+        resp = self.client.get(location, content_type="application/json")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_shopcart = resp.get_json()
+        self.assertEqual(new_shopcart["id"], shopcart.id, "Shopcart IDs do not match")
+        self.assertEqual(
+            new_shopcart["customer_id"],
+            shopcart.customer_id,
+            "Customer IDs do not match",
+        )
+        self.assertEqual(
+            new_shopcart["time_atc"], shopcart.time_atc, "Times do not match"
+        )
+
+    def test_update_shopcart(self):
+        """It should Update an existing shopcart"""
+        # create an shopcart to update
+        test_shopcart = ShopcartFactory()
+        resp = self.client.post(BASE_URL, json=test_shopcart.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the pet
+        new_shopcart = resp.get_json()
+        new_shopcart["customer_id"] = 1234567
+        new_shopcart_id = new_shopcart["id"]
+        resp = self.client.put(f"{BASE_URL}/{new_shopcart_id}", json=new_shopcart)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_shopcart = resp.get_json()
+        self.assertEqual(updated_shopcart["customer_id"], 1234567)
+
+    def test_delete_shopcart(self):
+        """It should Delete an shopcart"""
+        # get the id of an shopcart
+        shopcart = self._create_shopcarts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{shopcart.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_bad_request(self):
+        """It should not Create when sending the wrong data"""
+        resp = self.client.post(BASE_URL, json={"customer_id": "not enough data"})
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_unsupported_media_type(self):
+        """It should not Create when sending wrong media type"""
+        shopcart = ShopcartFactory()
+        resp = self.client.post(
+            BASE_URL, json=shopcart.serialize(), content_type="test/html"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.put(BASE_URL, json={"not": "today"})
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    ######################################################################
+    #  ITEM  T E S T   C A S E S
+    ######################################################################
+
+    def test_get_item_list(self):
+        """It should Get a list of items"""
+        # add two items to shopcart
+        shopcart = self._create_shopcarts(1)[0]
+        item_list = ItemFactory.create_batch(2)
+
+        # Create item 1
+        resp = self.client.post(
+            f"{BASE_URL}/{shopcart.id}/items", json=item_list[0].serialize()
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # Create item 2
+        resp = self.client.post(
+            f"{BASE_URL}/{shopcart.id}/items", json=item_list[1].serialize()
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # get the list back and make sure there are 2
+        resp = self.client.get(f"{BASE_URL}/{shopcart.id}/items")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        self.assertEqual(len(data), 2)
+
+    def test_add_item(self):
+        """It should Add an item to an shopcart"""
+        shopcart = self._create_shopcarts(1)[0]
+        item = ItemFactory()
+        resp = self.client.post(
+            f"{BASE_URL}/{shopcart.id}/items",
+            json=item.serialize(),
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # Make sure location header is set
+        location = resp.headers.get("Location", None)
+        self.assertIsNotNone(location)
+
+        data = resp.get_json()
+        logging.debug(data)
+
+        self.assertEqual(data["id"], item.id)
+        self.assertEqual(data["shopcart_id"], item.shopcart_id)
+        self.assertEqual(data["description"], item.description)
+        self.assertEqual(data["quantity"], item.quantity)
+        self.assertEqual(data["price"], item.price)
+
+        # Check that the location header was correct by getting it
+        resp = self.client.get(location, content_type="application/json")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        new_item = resp.get_json()
+        self.assertEqual(
+            new_item["quantity"], item.quantity, "item quantity does not match"
+        )
+
+    def test_get_item(self):
+        """It should Get an item from an shopcart"""
+        # create a known item
+        shopcart = self._create_shopcarts(1)[0]
+        item = ItemFactory()
+        resp = self.client.post(
+            f"{BASE_URL}/{shopcart.id}/items",
+            json=item.serialize(),
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        data = resp.get_json()
+        logging.debug(data)
+        item_id = data["id"]
+
+        # retrieve it back
+        resp = self.client.get(
+            f"{BASE_URL}/{shopcart.id}/items/{item_id}",
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        logging.debug(data)
+        self.assertEqual(data["id"], item.id)
+        self.assertEqual(data["shopcart_id"], item.shopcart_id)
+        self.assertEqual(data["description"], item.description)
+        self.assertEqual(data["quantity"], item.quantity)
+        self.assertEqual(data["price"], item.price)
+
+    def test_update_item(self):
+        """It should Update an item on an shopcart"""
+        # create a known item
+        shopcart = self._create_shopcarts(1)[0]
+        item = ItemFactory()
+        resp = self.client.post(
+            f"{BASE_URL}/{shopcart.id}/items",
+            json=item.serialize(),
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        data = resp.get_json()
+        logging.debug(data)
+        item_id = data["id"]
+        data["quantity"] = 20
+
+        # send the update back
+        resp = self.client.put(
+            f"{BASE_URL}/{shopcart.id}/items/{item_id}",
+            json=data,
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        # retrieve it back
+        resp = self.client.get(
+            f"{BASE_URL}/{shopcart.id}/items/{item_id}",
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        logging.debug(data)
+        self.assertEqual(data["id"], item_id)
+        self.assertEqual(data["shopcart_id"], shopcart.id)
+        self.assertEqual(data["quantity"], 20)
+
+    def test_delete_item(self):
+        """It should Delete an item"""
+        shopcart = self._create_shopcarts(1)[0]
+        item = ItemFactory()
+        resp = self.client.post(
+            f"{BASE_URL}/{shopcart.id}/items",
+            json=item.serialize(),
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        data = resp.get_json()
+        logging.debug(data)
+        item_id = data["id"]
+
+        # send delete request
+        resp = self.client.delete(
+            f"{BASE_URL}/{shopcart.id}/items/{item_id}",
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+        # retrieve it back and make sure item is not there
+        resp = self.client.get(
+            f"{BASE_URL}/{shopcart.id}/items/{item_id}",
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
---
+++ tests/test_shopcart.py
@@ -0,0 +1,247 @@
+######################################################################
+# Copyright 2016, 2024 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
+
+"""
+Test cases for Shopcart
+"""
+
+# pylint: disable=duplicate-code
+import os
+import logging
+from unittest import TestCase
+from unittest.mock import patch
+from wsgi import app
+from service.models import Shopcart, Item, DataValidationError, db
+from .factories import ShopcartFactory, ItemFactory
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
+)
+
+BASE_URL = "/shopcarts"
+
+
+######################################################################
+#  Shopcart   M O D E L   T E S T   C A S E S
+######################################################################
+# pylint: disable=too-many-public-methods
+class TestShopcart(TestCase):
+    """Test Cases for Shopcart Model"""
+
+    @classmethod
+    def setUpClass(cls):
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        app.app_context().push()
+
+    @classmethod
+    def tearDownClass(cls):
+        """This runs once after the entire test suite"""
+        db.session.close()
+
+    def setUp(self):
+        """This runs before each test"""
+        db.session.query(Shopcart).delete()  # clean up the last tests
+        db.session.query(Item).delete()  # clean up the last tests
+        db.session.commit()
+
+    def tearDown(self):
+        """This runs after each test"""
+        db.session.remove()
+
+    ######################################################################
+    #  T E S T   C A S E S
+    ######################################################################
+
+    def test_create_shopcart(self):
+        """It should create a Shopcart"""
+        shopcart = ShopcartFactory()
+        shopcart.create()
+        self.assertIsNotNone(shopcart.id)
+        found = Shopcart.all()
+        self.assertEqual(len(found), 1)
+        data = Shopcart.find(shopcart.id)
+        self.assertEqual(data.customer_id, shopcart.customer_id)
+        self.assertEqual(data.time_atc, shopcart.time_atc)
+
+    # Todo: Add your test cases here...
+
+    def test_create_a_shopcart(self):
+        """It should Create an shopcart and assert that it exists"""
+        fake_shopcart = ShopcartFactory()
+        # pylint: disable=unexpected-keyword-arg
+        shopcart = shopcart(
+            id=fake_shopcart.id,
+            customer_id=fake_shopcart.customer_id,
+            time_atc=fake_shopcart.time_atc,
+        )
+        self.assertIsNotNone(shopcart)
+        self.assertEqual(shopcart.id, fake_shopcart.id)
+        self.assertEqual(shopcart.customer_id, fake_shopcart.customer_id)
+        self.assertEqual(shopcart.time_atc, fake_shopcart.time_atc)
+
+    def test_add_a_shopcart(self):
+        """It should Create an shopcart and add it to the database"""
+        shopcarts = shopcart.all()
+        self.assertEqual(shopcarts, [])
+        shopcart = ShopcartFactory()
+        shopcart.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(shopcart.id)
+        shopcarts = shopcart.all()
+        self.assertEqual(len(shopcarts), 1)
+
+    @patch("service.models.db.session.commit")
+    def test_add_shopcart_failed(self, exception_mock):
+        """It should not create an shopcart on database error"""
+        exception_mock.side_effect = Exception()
+        shopcart = ShopcartFactory()
+        self.assertRaises(DataValidationError, shopcart.create)
+
+    def test_read_shopcart(self):
+        """It should Read an shopcart"""
+        shopcart = ShopcartFactory()
+        shopcart.create()
+
+        # Read it back
+        found_shopcart = shopcart.find(shopcart.id)
+
+        self.assertEqual(found_shopcart.id, shopcart.id)
+        self.assertEqual(found_shopcart.customer_id, shopcart.customer_id)
+        self.assertEqual(found_shopcart.time_atc, shopcart.time_atc)
+        self.assertEqual(found_shopcart.items, [])
+
+    def test_update_shopcart(self):
+        """It should Update an shopcart"""
+        shopcart = ShopcartFactory(customer_id=12345)
+        shopcart.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(shopcart.id)
+        self.assertEqual(shopcart.customer_id, 12345)
+
+        # Fetch it back
+        shopcart = shopcart.find(shopcart.id)
+        shopcart.customer_id = 123456
+        shopcart.update()
+
+        # Fetch it back again
+        shopcart = shopcart.find(shopcart.id)
+        self.assertEqual(shopcart.customer_id, 123456)
+
+    @patch("service.models.db.session.commit")
+    def test_update_shopcart_failed(self, exception_mock):
+        """It should not update an shopcart on database error"""
+        exception_mock.side_effect = Exception()
+        shopcart = ShopcartFactory()
+        self.assertRaises(DataValidationError, shopcart.update)
+
+    def test_delete_an_shopcart(self):
+        """It should Delete an shopcart from the database"""
+        shopcarts = Shopcart.all()
+        self.assertEqual(shopcarts, [])
+        shopcart = ShopcartFactory()
+        shopcart.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(shopcart.id)
+        shopcarts = shopcart.all()
+        self.assertEqual(len(shopcarts), 1)
+        shopcart = shopcarts[0]
+        shopcart.delete()
+        shopcarts = shopcart.all()
+        self.assertEqual(len(shopcarts), 0)
+
+    @patch("service.models.db.session.commit")
+    def test_delete_shopcart_failed(self, exception_mock):
+        """It should not delete an shopcart on database error"""
+        exception_mock.side_effect = Exception()
+        shopcart = ShopcartFactory()
+        self.assertRaises(DataValidationError, shopcart.delete)
+
+    def test_list_all_shopcarts(self):
+        """It should List all shopcarts in the database"""
+        shopcarts = Shopcart.all()
+        self.assertEqual(shopcarts, [])
+        for shopcart in ShopcartFactory.create_batch(5):
+            shopcart.create()
+        # Assert that there are not 5 shopcarts in the database
+        shopcarts = Shopcart.all()
+        self.assertEqual(len(shopcarts), 5)
+
+    def test_find_by_customer(self):
+        """It should Find an shopcart by customer id"""
+        shopcart = ShopcartFactory()
+        shopcart.create()
+
+        # Fetch it back by customer id
+        same_shopcart = shopcart.find_by_customer(shopcart.customer_id)[0]
+        self.assertEqual(same_shopcart.id, shopcart.id)
+        self.assertEqual(same_shopcart.customer_id, shopcart.customer_id)
+
+    def test_serialize_an_shopcart(self):
+        """It should Serialize an shopcart"""
+        shopcart = ShopcartFactory()
+        item = ItemFactory()
+        shopcart.items.append(item)
+        serial_shopcart = shopcart.serialize()
+        self.assertEqual(serial_shopcart["id"], shopcart.id)
+        self.assertEqual(serial_shopcart["customer_id"], shopcart.customer_id)
+        self.assertEqual(serial_shopcart["time_atc"], shopcart.time_atc)
+        self.assertEqual(len(serial_shopcart["items"]), 1)
+
+        items = serial_shopcart["items"]
+        self.assertEqual(items[0]["id"], item.id)
+        self.assertEqual(items[0]["name"], item.name)
+        self.assertEqual(items[0]["shopcart_id"], item.shopcart_id)
+        self.assertEqual(items[0]["description"], item.description)
+        self.assertEqual(items[0]["quantity"], item.quantity)
+        self.assertEqual(items[0]["price"], item.price)
+
+    def test_deserialize_an_shopcart(self):
+        """It should Deserialize an shopcart"""
+        shopcart = ShopcartFactory()
+        shopcart.items.append(ItemFactory())
+        shopcart.create()
+        serial_shopcart = shopcart.serialize()
+        new_shopcart = shopcart()
+        new_shopcart.deserialize(serial_shopcart)
+        self.assertEqual(new_shopcart.name, shopcart.name)
+        self.assertEqual(new_shopcart.userid, shopcart.userid)
+        self.assertEqual(new_shopcart.email, shopcart.email)
+        self.assertEqual(new_shopcart.phone_number, shopcart.phone_number)
+        self.assertEqual(new_shopcart.date_joined, shopcart.date_joined)
+
+    def test_deserialize_with_key_error(self):
+        """It should not Deserialize an shopcart with a KeyError"""
+        shopcart = Shopcart()
+        self.assertRaises(DataValidationError, shopcart.deserialize, {})
+
+    def test_deserialize_with_type_error(self):
+        """It should not Deserialize an shopcart with a TypeError"""
+        shopcart = Shopcart()
+        self.assertRaises(DataValidationError, shopcart.deserialize, [])
+
+    def test_deserialize_item_key_error(self):
+        """It should not Deserialize an item with a KeyError"""
+        item = Item()
+        self.assertRaises(DataValidationError, item.deserialize, {})
+
+    def test_deserialize_item_type_error(self):
+        """It should not Deserialize an item with a TypeError"""
+        item = Item()
+        self.assertRaises(DataValidationError, item.deserialize, [])
